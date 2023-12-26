3be24865b1295ce5eae80a8db5c69ef6af042c6e
KenPaolaRod <kenpaorin@gmail.com>
2023-12-26 17:07:50 -0500

    styles added to the  product detail page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Button({text, btnClass, onClick}) {
+function Button({text, btnClass, onClick, style}) {
   const buttonClasses = `btn ${btnClass || ''}`
   return (
-    <button className={buttonClasses} onClick={onClick}>{text}</button>
+    <button className={buttonClasses} onClick={onClick} style={style}>{text}</button>
   )
 }
 
--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -2,29 +2,18 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useContext } from 'react'
 import { ProductsContext } from '../contex/productsContext'
+import Button from './Button'
 
 
 
 function ProductDetail() {
   const productCtx = useContext(ProductsContext);
   const {products} = productCtx;
-  const ProductId = useParams()
+  const ProductId = useParams();
 
-
-  console.log(ProductId.productId);
-
-  // identificar cual es el id del producto que voy a mostrar
+  // identify what is the id of the product to be displayed
   const product = Array.isArray(products) ? products.find(product => product._id === ProductId.productId) : [];
-  // const sizes =  Array.isArray(product.sizes) ? product
-
-  
-
-
-
-  // console.log(sizes);
-
-  console.log(product);
-
+  const productSize =  product.sizes;
 
   return (
     <article className='product-datails'>
@@ -36,12 +25,28 @@ function ProductDetail() {
 
         <div className="product-color">
           <h2>Color</h2>
-          <span style={{backgroundColor: product.colors}}>{product.colors}</span>
+          <div>
+            {product.colors ? product.colors.map((color, index) => (
+              <Button style={{backgroundColor: color}} btnClass='color' key={index}  />
+              )) : <p>No colors to be show</p> }
+          </div>
         </div>
 
         <div className="product-size">
-          <h2>{product.sizes.join(" ")}</h2>
+          <h2>Size</h2>
+          <div>{productSize ? productSize.map((size, index) => (
+            <Button key={index} text={size} btnClass='size'  />
+          )) : <p>There is not sizes to show</p> }
+          </div>
         </div>
+
+        <div className="price">
+          <h2>Price</h2>
+          <span>${product.price}</span>
+        </div>
+
+        <Button btnClass='addToCard' text='add to cart' />
+
       </div>
 
     </article>
