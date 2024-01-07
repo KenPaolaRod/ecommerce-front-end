9b70c1e31b8573f8626c4df69fa16bb7a5d876aa
KenPaolaRod <kenpaorin@gmail.com>
2024-01-06 21:44:09 -0500

    footer img added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ import { FaLinkedin } from "react-icons/fa6";
 function Footer() {
   return (
     <footer>
-      <img src="/public/images/Kencool-logo.png" alt="logo" />
+      <img src="/images/Kencool-logo.png" alt="logo" />
       <FaSquareInstagram className='f-icon' />
       <FaFacebookSquare className='f-icon' />
       <FaLinkedin className='f-icon' />
